961de0f482c58bc13dcf5260045d50597c0a514d
Pedro Castro <pdesacastro@gmail.com>
2021-06-22 16:17:35 +0000

    update

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 server:
 	bundle exec jekyll serve
+
+bundle:
+	./node_modules/.bin/esbuild assets/js/main.js --minify --outfile=assets/js/main.min.js
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,43 @@
 (() => {
-  const post_content = document.querySelector(".post-content")
-  
-  if (post_content === null) {
-    return;
-  }
+  const codeBlocks = document.querySelectorAll('div[class*="language-"]');
+  codeBlocks !== null && [...codeBlocks].map((element) => {
+    const langName = element.className.match(/\blang(?:uage)?-([\w-]+)\b/i);
+    langName !== null && element.setAttribute('data-language', langName[1]);
 
-  Array.from(post_content.querySelectorAll("h1, h2, h3, h4, h5, h6")).map((element) => {
+    // Clipboard button
+    if (navigator.clipboard) {
+      const codeContent = element.querySelector('code');
+      if (codeContent === null || codeContent.textContent === null) {
+        return;
+      }
+      const buttonClip = document.createElement('button');
+      buttonClip.setAttribute("type", "button");
+      buttonClip.setAttribute("aria-label", "Copy to clipboard");
+      buttonClip.textContent = "📋";
+      element.appendChild(buttonClip);
+
+      buttonClip.addEventListener('click', () => {
+        navigator.clipboard.writeText(codeContent.textContent)
+        .then(() => {
+          buttonClip.classList.add('copy-sucess');
+        })
+        .catch(err => {
+          console.log(err);
+          buttonClip.classList.add('copy-error');
+        });
+        setTimeout(() => {
+          buttonClip.className = "";
+        }, 1000)
+      });
+    }
+  });
+  
+  const postContent = document.querySelector(".post-content");
+  postContent !== null && [...postContent.querySelectorAll("h1, h2, h3, h4, h5, h6")].map((element) => {
     const anchor = document.createElement("a");
     anchor.textContent = "#";
     anchor.setAttribute("href", "#" + element.getAttribute("id"));
     element.appendChild(anchor);
   });
-
-  Prism.plugins.toolbar.registerButton('Language', (env) => {
-    const span = document.createElement("span");
-    span.textContent = env.language;
-    return span;
-  });
-
-  if (navigator.clipboard) {
-    Prism.plugins.toolbar.registerButton('Copy', (env) => {
-      const copyButton = document.createElement('button');
-      copyButton.setAttribute("type", "button");
-      copyButton.setAttribute("aria-label", "Copy");
-      copyButton.textContent = "📋";
-
-      copyButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(env.code)
-        .then(() => {
-          copyButton.classList.add('copy-sucess');
-        })
-        .catch(err => {
-          console.log(err);
-          copyButton.classList.add('copy-error');
-        });
-        setTimeout(() => {
-          copyButton.className = "";
-        }, 1000)
-      });
-
-      return copyButton;
-    });
-  }
 
 })();
